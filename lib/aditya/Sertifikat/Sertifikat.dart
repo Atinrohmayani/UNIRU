1f24c3396dbc72567a8254c77eb9efcd6c149eca
aditya2708 <adityahadywijaya@gmail.com>
2023-04-05 12:44:39 +0700

    Aditya menambahkan fitur Sertifikat

---
+++ lib/aditya/Sertifikat/Sertifikat.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30.0,
+          ),
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                     
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Sertifikat yg dimiliki',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+              
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              courseLayout(context)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget courseLayout(BuildContext context) {
+  List<String> imageFileList = [
+    'go.png',
+    'nodejs.png',
+    'flutter.png',
+    'dart.png',
+    'react.png',
+    'vue.png',
+    'firebase.png',
+    'postgres.png',
+  ];
+
+  return MasonryGridView.count(
+    shrinkWrap: true,
+    physics: const NeverScrollableScrollPhysics(),
+    crossAxisCount: 2,
+    mainAxisSpacing: 27,
+    crossAxisSpacing: 23,
+    itemCount: imageFileList.length,
+    itemBuilder: (context, index) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Image.asset(
+          'assets/images/${imageFileList[index]}',
+        ),
+      );
+    },
+  );
+}
